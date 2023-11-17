be19053e0307444bb17cb0c4b67d9bd6098c49b0
Ryan Valera <valeraryan@gmail.com>
2023-11-17 14:42:40 -0800

    Create start-timer logic when 'Start Challenge'' button is clicked. A dynamic display mes sage is created to toggle between 'Time is running ...' and 'Timer inactive' as well as a 'You lost' message when the timer runs out.

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,17 +1,35 @@
+import {useState, useRef} from 'react';
+
 function TimerChallenge({title, targetTime}) {
+    const [timerExpired, setTimerExpired] = useState(false);
+    const [timerStart, setTimerStart] = useState(false);
+
+    // Multiply by 1000 to calculate in milliseconds
+    function startChallengeHandler() {
+        setTimerStart(true);
+        setTimeout(() => {
+            setTimerExpired(true);
+            setTimerStart(false);
+        }, targetTime * 1000);
+    }
+
+    const gameStatusMessage = timerExpired && <p>You Lost!</p>
+    const startTimeMessage = timerStart ? <p>Time is running ...</p> : <p>Timer inactive</p>
+
     return (
         <section className="challenge">
             <h2>{title}</h2>
+            {gameStatusMessage}
             <p className="challenge-time">
                 {targetTime} second{targetTime > 1 ? 's' : ''}
             </p>
             <p>
-                <button>
+                <button onClick={startChallengeHandler}>
                     Start Challenge
                 </button>
             </p>
             <p className="">
-                Time is running ... / Timer inactive
+                {startTimeMessage}
             </p>
         </section>
     );
