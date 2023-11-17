ddc3af2418bd4929bb032a45c1d29f83b17c1dd3
Ryan Valera <valeraryan@gmail.com>
2023-11-16 16:10:38 -0800

    Add functionality to Player.jsx component to dynamically display a player caption there is an input submission upon button click event.

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,34 @@
+import {useState, useRef} from 'react';
+
 export default function Player() {
-  return (
-    <section id="player">
-      <h2>Welcome unknown entity</h2>
-      <p>
-        <input type="text" />
-        <button>Set Name</button>
-      </p>
-    </section>
-  );
+    const playerNameInput = useRef();
+
+    const [playerName, setPlayerName] = useState('');
+    // const [submitted, setSubmitted] = useState(false);
+
+    // function changeNameHandler(e) {
+    //     setSubmitted(false);
+    //     setPlayerName(e.target.value)
+    // }
+
+    function editNameClickHandler() {
+        setPlayerName(playerNameInput.current.value);
+        playerNameInput.current.value = '';
+    }
+
+
+    return (
+        <section id="player">
+            <h2>Welcome {playerName ?? 'unknown entity'}</h2>
+            <p>
+                <input
+                    ref={playerNameInput}
+                    // onChange={(event) => changeNameHandler(event)}
+                    type="text"
+                    // value={playerName}
+                />
+                <button onClick={editNameClickHandler}>Set Name</button>
+            </p>
+        </section>
+    );
 }
