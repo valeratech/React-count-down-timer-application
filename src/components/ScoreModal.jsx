38f0171109c43d6745d63d534b32235d7f9fa4d3
Ryan Valera <valeraryan@gmail.com>
2023-11-18 10:44:13 -0800

    Create ScoreModal.jsx component to alert user with a pop up modal when time runs out and display the game status and score.

---
+++ src/components/ScoreModal.jsx
@@ -0,0 +1,23 @@
+import {forwardRef} from "react";
+
+// forwardRef is a helper function from React that allows us to wrap/forward a component's ref to another one.
+const ScoreModal = forwardRef(function ScoreModal({targetTime, result}, ref) {
+    return (
+        // Instead of forwarding a ref to a DOM node, you can forward it to your own component
+        <dialog ref={ref} className="result-modal">
+            <h2>You {result}</h2>
+            <p>The target time was <strong>{targetTime} seconds.</strong></p>
+            <p>You stopped the timer with <strong>X seconds left.</strong></p>
+            {/*The <dialog> HTML element represents a modal or non-modal dialog box or other interactive component, */}
+            {/*such as a dismissible alert, inspector, or subwindow.*/}
+            <form method="dialog">
+                {/*The dialog box can be closed using the .close() method or using the dialog method when submitting */}
+                {/*a <form> that is nested within the <dialog> element. Modal dialogs can also be closed by pressing */}
+                {/*the Esc key.*/}
+                <button>Close</button>
+            </form>
+        </dialog>
+    );
+});
+
+export default ScoreModal;
--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,7 +1,10 @@
 import {useState, useRef} from 'react';
+import ScoreModal from "./ScoreModal.jsx";
 
 function TimerChallenge({title, targetTime}) {
     const timerId = useRef();
+    const dialog = useRef();
+
     const [timerExpired, setTimerExpired] = useState(false);
     const [timerStart, setTimerStart] = useState(false);
 
@@ -17,28 +20,34 @@ function TimerChallenge({title, targetTime}) {
         timerId.current = setTimeout(() => {
             setTimerStart(false);
             setTimerExpired(true);
+            dialog.current.showModal()
         }, targetTime * 1000);
     }
 
-    const gameStatusMessage = timerExpired && <p>You Lost!</p>
+    // Replace with modal
+    // const gameStatusMessage = timerExpired && <p>You Lost!</p>
     const startTimeMessage = timerStart ? <p>Time is running ...</p> : <p>Timer inactive</p>
 
     return (
-        <section className="challenge">
-            <h2>{title}</h2>
-            {gameStatusMessage}
-            <p className="challenge-time">
-                {targetTime} second{targetTime > 1 ? 's' : ''}
-            </p>
-            <p>
-                <button onClick={!timerStart ? startChallengeHandler : stopChallengeHandler}>
-                    {`${!timerStart ? 'Start' : 'Stop'} Challenge`}
-                </button>
-            </p>
-            <p className="">
+        <>
+            {/*Instead of forwarding a ref to a DOM node, you can forward it to your own component*/}
+            {/*{timerExpired && <ScoreModal ref={dialog} targetTime={targetTime} result="lost" />}*/}
+            {/*TimeExpired Boolean no longer required*/}
+            <ScoreModal ref={dialog} targetTime={targetTime} result="lost" />
+            <section className="challenge">
+                <h2>{title}</h2>
+                {/*{gameStatusMessage}*/}
+                <p className="challenge-time">
+                    {targetTime} second{targetTime > 1 ? 's' : ''}
+                </p>
+                <p>
+                    <button onClick={!timerStart ? startChallengeHandler : stopChallengeHandler}>
+                        {`${!timerStart ? 'Start' : 'Stop'} Challenge`}
+                    </button>
+                </p>
                 {startTimeMessage}
-            </p>
-        </section>
+            </section>
+        </>
     );
 }
 
